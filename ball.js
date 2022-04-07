7ec2a519240b96272d43bd13a91410ec9e3be027
Randy King Jr <rkingjr@outlook.com>
2022-04-06 20:48:58 -0500

    add deflection off paddles

--- ball.js
+++ ball.js
@@ -40,7 +40,7 @@ export default class Ball {
         this.velocity = initialVelocity;
     }
 
-    update(delta) {
+    update(delta, paddleRects) {
         this.x += this.direction.x * this.velocity * delta;
         this.y += this.direction.y * this.velocity * delta;
         this.velocity += velocityIncrease * delta
@@ -48,7 +48,7 @@ export default class Ball {
         if (rect.bottom >= window.innerHeight || rect.top <= 0) {
             this.direction.y *= -1
         }
-        if (rect.right >= window.innerWidth || rect.left <= 0) {
+        if (paddleRects.some(r => isCollision(r, rect))) {
             this.direction.x *= -1
         }
     }
@@ -56,4 +56,13 @@ export default class Ball {
 
 function randomNumberBetween(min, max) {
     return Math.random() * (max - min) + min;
+}
+
+function isCollision(rect1, rect2) {
+    return (
+        rect1.left <= rect2.right &&
+        rect1.right  >=  rect2.left &&
+        rect1.top <= rect2.bottom &&
+        rect1.bottom >= rect2.top
+    )
 }
--- script.js
+++ script.js
@@ -4,17 +4,36 @@ import Paddle from "./Paddle.js "
 const ball = new Ball(document.getElementById("ball"))
 const playerPaddle = new Paddle(document.getElementById("player-paddle"))
 const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
-
+const playerScoreElement = document.getElementById("play-score")
+const computerScoreElement = document.getElementById("computer-score")
 
 let lastTime
 function update(time) {
     if (lastTime != null) {
         const delta = time - lastTime
-        ball.update(delta)
+        ball.update(delta, [playerPaddle.rect(), computerPaddle.rect() ])
         computerPaddle.update(delta, ball.y)
+
+        if (hasLost()) handleLost()
     }
     lastTime = time
     window.requestAnimationFrame(update)
+}
+
+function handleLost() {
+    const rect = ball.rect()
+    if  (rect.right >= window.innerWidth) {
+        playerScoreElement.textContent = parseInt(playerScoreElement.textContent) + 1
+    }   else {
+        computerScoreElement.textContent = parseInt(computerScoreElement.textContent) + 1
+    }
+    ball.reset()
+    computerPaddle.reset()
+}
+
+function hasLost() {
+    const rect = ball.rect()
+    return rect.right >= window.innerWidth || rect.left <= 0
 }
 
 document.addEventListener("mousemove", e => {
